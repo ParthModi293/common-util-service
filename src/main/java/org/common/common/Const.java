6c28f5ec9f6720527c6c646ac08a058464efc027
mtfive001 <mt5fdev1@outlook.com>
2025-01-24 11:53:59 +0530

    Initial commit

---
+++ src/main/java/org/common/common/Const.java
@@ -0,0 +1,4 @@
+package org.common.common;
+
+public class Const {
+}
---
+++ src/main/java/org/common/common/Enum.java
@@ -0,0 +1,40 @@
+package org.common.common;
+
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = {EnumValueValidator.class})
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Enum {
+
+    enum Y_N {
+        Y("Y"),
+        N("N");
+
+
+        private final String value;
+
+        Y_N(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+
+    String message() default "Invalid value!";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    Class<? extends java.lang.Enum<?>> enumClass();
+
+    boolean ignoreCase() default false;
+}
