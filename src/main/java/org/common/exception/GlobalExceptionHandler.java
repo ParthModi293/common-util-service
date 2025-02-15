601bebb3744cbb0dd0b1dad0661c58e38dda5a47
mtfive001 <mt5fdev1@outlook.com>
2025-02-15 16:04:26 +0530

    commit const file

--- src/main/java/org/common/exception/GlobalExceptionHandler.java
+++ src/main/java/org/common/exception/GlobalExceptionHandler.java
@@ -1,9 +1,16 @@
 package org.common.exception;
 
+import org.common.common.Const;
+import org.common.common.ResponseBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
@@ -11,11 +18,42 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity<Object> handleValidationException(ValidationException ex) {
-        return new ResponseEntity<>(Map.of(
-                "rCode", ex.getRCode(),
-                "rStatus", ex.getRStatus(),
-                "displayMessage", ex.getDisplayMessage(),
-                "rMsg", ex.getRMsg()
-        ), ex.getRStatus());
+        Map<String, Object> response = new HashMap<>();
+        response.put("rCode", ex.getRCode());
+        response.put("rStatus", ex.getRStatus().value());
+        response.put("displayMessage", ex.getDisplayMessage());
+        response.put("rMsg", ex.getRMsg());
+
+        return new ResponseEntity<>(response, ex.getRStatus());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseBean<Map<String, String>>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        BindingResult bindingResult = ex.getBindingResult();
+
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error : bindingResult.getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+
+        ResponseBean<Map<String, String>> errorResponse = new ResponseBean<>();
+        errorResponse.setRStatus(HttpStatus.BAD_REQUEST);
+        errorResponse.setRMsg("Validation Failed");
+        errorResponse.setRCode(Const.rCode.BAD_REQUEST);
+        errorResponse.setDisplayMessage(errors.toString());
+
+        return new ResponseEntity<>(errorResponse, errorResponse.getRStatus());
+    }
+
+
+    @ExceptionHandler(FileNotFoundException.class)
+    public ResponseEntity<Object> handleFileNotFoundException(FileNotFoundException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("rCode", ex.getRCode());
+        response.put("rStatus", ex.getRStatus().value());
+        response.put("displayMessage", ex.getDisplayMessage());
+        response.put("rMsg", ex.getRMsg());
+
+        return new ResponseEntity<>(response, ex.getRStatus());
     }
 }
